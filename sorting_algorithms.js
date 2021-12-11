f34a3bd461466acccdbd95f08d80ac6a20fa4cc5
Kseniya Yarosh <okyarosh@ukr.net>
2021-12-10 23:56:24 -0500

    Update sorting_algorithms.js

--- sorting_algorithms.js
+++ sorting_algorithms.js
@@ -16,3 +16,25 @@ function bubbleSort(array) {
 
 bubbleSort(numArray);
 console.log(numArray);
+
+
+
+const numArray2 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function selectionSort(array) {
+  const length = array.length;
+  for(let i = 0; i < length; i++){
+    // set current index as minimum
+    let min = i;
+    let temp = array[i];
+    for(let j = i+1; j < length; j++){
+      if (array[j] < array[min]){
+        //update minimum if current is lower that what we had previously
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+}
