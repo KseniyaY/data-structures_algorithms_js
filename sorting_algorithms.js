55f17132a59394e46a82ee56a651813df981d07b
Kseniya Yarosh <okyarosh@ukr.net>
2021-12-14 22:31:26 -0500

    Update sorting_algorithms.js

--- sorting_algorithms.js
+++ sorting_algorithms.js
@@ -91,5 +91,23 @@ function mergeSort (array) {
   )
 }
 
+function merge(left, right){
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while(leftIndex < left.length && 
+        rightIndex < right.length){
+     if(left[leftIndex] < right[rightIndex]){
+       result.push(left[leftIndex]);
+       leftIndex++;
+     } else{
+       result.push(right[rightIndex]);
+       rightIndex++
+    }
+  }  
+  // console.log(left, right)
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
 mergeSort(numArray4);
 console.log(numArray4);
