e9df07d985bee2617c99eadd3d0efd138ddb7f29
Kseniya Yarosh <okyarosh@ukr.net>
2021-12-14 22:29:50 -0500

    Update sorting_algorithms.js

--- sorting_algorithms.js
+++ sorting_algorithms.js
@@ -69,3 +69,27 @@ function insertionSort(array) {
 
 insertionSort(numArray3);
 console.log(numArray3);
+
+const numArray4 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort (array) {
+  if (array.length === 1) {
+    return array
+  }
+  // Split Array in into right and left
+  const length = array.length;
+  const middle = Math.floor(length / 2)
+  const left = array.slice(0, middle) 
+  const right = array.slice(middle)
+  // console.log('left:', left);
+  // console.log('right:', right);
+
+  
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
+}
+
+mergeSort(numArray4);
+console.log(numArray4);
